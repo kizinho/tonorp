8af0b29cdfb04b6d04aee1fe0bb292cf35911bfe
Kizinho <adikekizinho@gmail.com>
2020-08-19 14:34:11 +0100

    fix metttig

---
+++ src/controllers/meeting.js
@@ -0,0 +1,17 @@
+const {
+    meeting
+} = require('../../models/index')
+
+const userMeeting = async (attendanceGroupsId, type) => {
+    if (!attendanceGroupsId || !type || typeof attendanceGroupsId !== 'number' || typeof type !== 'string') {
+        throw new Error('Attendance group and type can not be null');
+    }
+    const groupMeeting = await meeting.create({
+        attendanceGroupsId,
+        type
+    })
+    return groupMeeting
+}
+
+
+module.exports = userMeeting
