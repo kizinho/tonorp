1462be750ee9c50f06b21393dec0f97d0e826dcd
Melody Daniel <melodyogonna@gmail.com>
2020-09-04 16:38:59 +0100

    minor change

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const app = require('express')();
 
 app.use('/api/v1/attendance', require('./routes/attendance'));
-app.use('/api/v1/user', require('./routes/user'));
+app.use('/api/v1/users', require('./routes/user'));
 
 module.exports = app;
