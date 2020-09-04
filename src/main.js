68ea5448c8ae5a4a8fb7bcb05d9783da70698fc7
Melody Daniel <melodyogonna@gmail.com>
2020-09-04 16:00:19 +0100

    Routing basics

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 const app = require('express')();
 
 app.use('/api/v1/attendance', require('./routes/attendance'));
+app.use('/api/v1/user', require('./routes/user'));
 
 module.exports = app;
