f6aaa9694dea75713a95a9428bfd7956ff7a42f5
melodyogonna <melodyogonna@gmail.com>
2020-09-09 14:32:30 +0100

    Revert "Melody"

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 const model = require('../../models/index');
 const Validate = require('../modules/utils/ValidateUser');
-const { hash } = require('../modules/utils/hashPassword');
+const { hash } = require('../modules/utils/hashPassword')
 
 // Validates and registers a user to the app
 const addUser = async (userDetails, user = model.User) => {
@@ -10,16 +10,17 @@ const addUser = async (userDetails, user = model.User) => {
   }
 
   const validateuser = new Validate(userDetails);
+ 
 
   if (!validateuser.validate()) {
     throw new Error('Invalid user details');
   }
 
   try {
-    const password = await hash(userDetails.password);
+    const password =  await hash(userDetails.password);
     // eslint-disable-next-line no-param-reassign
-    userDetails.password = password;
-
+    userDetails.password = password
+      
     const new_user = await user.create(userDetails);
     return new_user;
   } catch (e) {
@@ -29,14 +30,4 @@ const addUser = async (userDetails, user = model.User) => {
   }
 };
 
-const returnUser = async (user_id) => {
-  if (!user_id || typeof user_id !== 'number') {
-    throw TypeError(
-      `Invalid user Id type, expected number, got ${typeof user_id}}`
-    );
-  }
-
-  const user = await model.user.findByPk(user_id);
-  return user;
-};
-module.exports = { addUser, returnUser };
+module.exports = { addUser };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,31 +1,13 @@
 // Third party modules
 require('dotenv');
+const { sequelize: db } = require('sequelize');
 const express = require('express');
 
 // Application modules
-const userController = require('../controllers/users');
+const attendanceController = require('../../controllers/attendanceController');
 
-// Initialize express router
 const router = express.Router();
 
-// Initialize middlewares
-router.use(express.json());
+router.get('user/:userID', attendanceController.userAttendances);
 
-// Define routes
-router.get('/:user_id', async (request, response) => {
-  const { user_id } = request.params;
-
-  if (typeof parseInt(user_id, 10) !== 'number') {
-    response.status(400).json({ error: 'Invalid user id type' });
-  }
-
-  const user = userController.returnUser(parseInt(user_id));
-  return response.send(user);
-});
-
-router.post('/register', async (request, response) => {
-  const user = await userController.addUser(request.body);
-  return response.send(user);
-});
-
-module.exports = router;
+export default router;
