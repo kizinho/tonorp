bed7c9593ca8829cbee7427fe787d10c0b814630
Kizinho <adikekizinho@gmail.com>
2020-09-09 14:43:21 +0100

    new router

--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -1,8 +1,15 @@
 /* eslint-disable camelcase */
-const router = require('express').Router();
+const express = require('express')
+const router = express.Router();
 
 // Application modules
 const { userGroups } = require('../controllers/getUserGroups');
+const { addGroup } = require('../controllers/groups');
+const { requests } = require('sinon');
+const { response } = require('express');
+
+// Initialize middlewares
+router.use(express.json());
 
 router.get('/user-groups/:userId', async (request, response) => {
   const { userId } = request.params;
@@ -10,10 +17,28 @@ router.get('/user-groups/:userId', async (request, response) => {
   if (typeof parseInt(userId, 10) !== 'number') {
     return response.status(400).json({ error: 'userId should be a number' });
   }
+  try {
+    const user_groups = await userGroups(parseInt(userId, 10));
+    return response.send(user_groups);
+  } catch (error) {
+    return response.status(400).json({ error: error.message })
+  }
+});
 
-  const user_groups = await userGroups(parseInt(userId, 10));
-  console.log(user_groups);
-  return response.send('user attendance');
+router.post('/create', async (request, response) => {
+  const {name, ownerId} = request.body
+  try {
+    const group = await addGroup(ownerId, name);
+    return response.send(group);
+  } catch (error) {
+    return response.status(400).json({ error: error.message })
+  }
+
+});
+
+router.post('/meeting' , async (request, response) => {
+
+
 });
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,6 @@ const express = require('express');
 
 // Application modules
 const userController = require('../controllers/users');
-const getUserGroupController = require('../controllers/getUserGroups');
 
 // Initialize express router
 const router = express.Router();
@@ -26,20 +25,13 @@ router.get('/:user_id', async (request, response) => {
 });
 
 router.post('/register', async (request, response) => {
+  try{
   const user = await userController.addUser(request.body);
   return response.send(user);
-});
-
-router.get('/get_groups/:user_id', async (request, response) => {
-  const { user_id } = requests.params
-  try {
-    const getGroup = await getUserGroupController.userGroups(parseInt(user_id));
-    return response.send(getGroup);
   } catch (error) {
-    console.log(error.message)
-    return response.status(400).json({ error: 'Error Ocuured' })
+    return response.status(400).json({ error: error.message })
   }
-
 });
+
 
 module.exports = router;
