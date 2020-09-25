23129b34cc35ee9b54f8380d7573d531d96c2d17
Melody Daniel <melodyogonna@gmail.com>
2020-09-25 11:58:40 +0100

    Route consistency

--- src/routes/attendance.js
+++ src/routes/attendance.js
@@ -4,14 +4,11 @@ const router = require('express').Router();
 const passport = require('../middlewares/auth/index');
 const { userAttendances } = require('../controllers/attendance');
 
-router.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    console.log(req.user);
-    const attendances = await userAttendances(req.user.id);
-    return res.send(req.user);
-  }
-);
+const authenticateUser = passport.authenticate('jwt', { session: false });
+
+router.get('/', authenticateUser, async (req, res) => {
+  const attendances = await userAttendances(req.user.id);
+  return res.send(req.user);
+});
 
 module.exports = router;
