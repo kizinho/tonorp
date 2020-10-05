34c8b69f792ad41587757fc51fd7d6f1729e4e4f
Kizinho <adikekizinho@gmail.com>
2020-10-05 13:55:52 +0100

    forgot password

--- test/controllers/testMeetingController.js
+++ test/controllers/testMeetingController.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = 'test';
 const { expect } = require('chai');
 
 const { User, attendanceGroups, meeting } = require('../../models/index');
-const { userMeeting } = require('../../src/controllers/meeting');
+const { userMeeting, meetingSort } = require('../../src/controllers/meeting');
 const { generateRandomString } = require('../../src/modules/utils/helper');
 
 const data = {
@@ -62,5 +62,61 @@ describe('Test Meeting controllers', () => {
   it('should test that meeting is created', async () => {
     const meetings = await userMeeting(attendGroupId, 'physical');
     expect(typeof meetings.id).to.equal('number');
+  });
+});
+
+describe('Test All Meeting sort controllers', () => {
+  let userId;
+  let attendanceGroupId;
+  before(async () => {
+    const testUser = await User.create(data);
+    userId = testUser.id;
+
+    const testAttendGroup = await attendanceGroups.create({
+      name: 'workshop',
+      ownerId: userId,
+      groupId: generateRandomString(7),
+    });
+    attendanceGroupId = testAttendGroup.id;
+    await meeting.create({
+      attendanceGroupId,
+      type: 'visual'
+    });
+
+    return testUser;
+  });
+  after((done) => {
+    attendanceGroups
+      .destroy({
+        truncate: {
+          cascade: true,
+          restartIdentity: true,
+        },
+      })
+      .then(() => {
+        User.destroy({
+          truncate: {
+            cascade: true,
+            restartIdentity: true,
+          },
+        });
+      })
+      .then(() => {
+        meeting
+          .destroy({
+            truncate: {
+              cascade: true,
+              restartIdentity: true,
+            },
+          })
+          .then(() => done());
+      })
+      .catch(() => {
+        done();
+      });
+  });
+  it('should test that meeting sort is received', async () => {
+    const sort = await meetingSort(attendanceGroupId, new Date);
+    expect(sort).to.be.an('array');
   });
 });
